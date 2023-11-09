449a235a98a54d2f120133892f54c19399a132ec
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-11-09 14:13:34 +0100

    chore: formatting

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $1
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
+STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l)
 if test "$BRANCH" != ""; then
   if test "$STATUS" = "0"; then
     echo "#[fg=green,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
