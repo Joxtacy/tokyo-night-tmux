afdacb852c82dbde9f702d5422cde715fb2d7020
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-11-09 16:39:24 +0100

    feat!: make it an option to show git status

--- src/git-status.sh
+++ src/git-status.sh
@@ -2,7 +2,9 @@
 cd $1
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l)
-if [ "$BRANCH" != "" ]; then
+SHOW_GIT_STATUS_OPTION="@tokyonight-show-git-status"
+SHOW=$(tmux show-option -gqv "$SHOW_GIT_STATUS_OPTION")
+if [ "$BRANCH" != "" ] && [ "$SHOW" != "" ]; then
   if [ "$STATUS" -eq "0" ]; then
     echo "#[fg=green,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
   else
